3136598ce70e9b34304af5713e6a5255c89edff3
Natalie Montoya <nataliesbarron@gmail.com>
2017-12-29 07:57:59 -0700

    React static comp

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
-  // WHAT GOES HERE?
+const Footer = (props) => {
+  return (
+    <div className='Footer'>
+      <div className='footerTitle'>
+        
+        <h3><img src='../public/copyright.svg'/> 2017</h3>
+      </div>
+    </div>    
+    )
 }
 
 export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className='Header'>
-      {/* What goes in here? */}
+      <div className='headerTitle'>
+        <a href='#'>Be Extra Membership Info</a>
+        <h3>{props.headerTitle}</h3>
+        <div className='signInBtn'>
+          <img src="../public/search.svg"/>
+          <a href='#'>Sign in</a>
+          <button> Get Started </button>
+        </div>
+      </div>
     </div>
   )
 }
