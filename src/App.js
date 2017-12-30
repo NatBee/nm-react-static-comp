1b3509f4d2692679e74a262ddb84dae87f808388
Natalie Montoya <nataliesbarron@gmail.com>
2017-12-30 14:55:20 -0700

    Add more pics

--- src/App.js
+++ src/App.js
@@ -7,33 +7,68 @@ import './App.css';
 
 const stories = [
   {
-    img: './public/story1.jpg',
-    title: 'Winning at Fonts',
-    desc: 'or, Why you should care about typography',
+    img: './public/boss.jpg',
+    title: 'How to be a Boss',
+    desc: 'A guide for women on how to be a boss. Mansplained by Donald Trump.',
     authImg: './public/louisa.png',
     authName: 'Louisa',
     estTime: '2 min'
   },
   {
-    img: './public/story2.jpg',
-    title: 'Hoodlums: a dissertation',
-    desc: 'Examining villains in modern storytelling',
+    img: './public/cat.jpg',
+    title: 'Make Better Decisions',
+    desc: 'Your cat sees everything and YES he\'s judging you!',
     authImg: './public/alternate.png',
     authName: 'Nathaniel',
     estTime: '10 min'
   },
   {
-    img: './public/story3.jpg',
-    title: 'Halloween is a year-round sport',
-    desc: 'Those who say otherwise just aren\'t trying hard enough.',
+    img: './public/shoes.jpg',
+    title: 'Walk a Mile in My Shoes',
+    desc: 'One woman\'s quest to master badass activities while wearing whatever the F#*$ she wants.',
     authImg: './public/leta.png',
     authName: 'Leta',
     estTime: '8 min'
   },
   {
-    img: './public/story4.jpg',
-    title: 'How to Improve Learning and Classroom Engagement with Humor',
-    desc: 'Okay, so first of all, everybody knows fart jokes are hilarious ...',
+    img: './public/eyes.jpg',
+    title: 'How to Land Your Dream Job',
+    desc: 'Build your personal brand to stand out and get your dream job.',
+    authImg: './public/pamela.png',
+    authName: 'Pamela',
+    estTime: '11 min'
+  }
+]
+
+const stories2 = [
+  {
+    img: './public/heart.jpg',
+    title: 'Heart',
+    desc: 'A memoir of love, fashion, and friendship.',
+    authImg: './public/louisa.png',
+    authName: 'Louisa',
+    estTime: '2 min'
+  },
+  {
+    img: './public/drown.jpg',
+    title: 'Shine On',
+    desc: 'Keep your light buring bright.',
+    authImg: './public/alternate.png',
+    authName: 'Nathaniel',
+    estTime: '10 min'
+  },
+  {
+    img: './public/unicorn.jpg',
+    title: 'Uniquely Yours',
+    desc: 'A lonely Rhino\'s journey of self acceptance.',
+    authImg: './public/leta.png',
+    authName: 'Leta',
+    estTime: '8 min'
+  },
+  {
+    img: './public/watch.jpg',
+    title: 'Time Travel',
+    desc: 'The cautious persuit of time travel and the world of the future.',
     authImg: './public/pamela.png',
     authName: 'Pamela',
     estTime: '11 min'
@@ -48,7 +83,7 @@ const App = () => {
       <Stories  storiesTitle="Today's Top Stories"
                 stories={stories} />
       <Stories  storiesTitle="Pop Culture"
-                stories={stories} />
+                stories={stories2} />
       <Footer/>
     </div>
   );
